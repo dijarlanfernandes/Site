3fbe9e152fda2e31a859d82e28225d3306b4b389
dijarlanfernandes <81352231Dija>
2022-09-10 10:54:05 -0300

    start commit

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio_Dev.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio_Dev.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel Login)
+        {
+            
+            if (!ModelState.IsValid)
+                 return View(Login);
+
+                var user = await _userManager.FindByNameAsync(Login.UserName);
+            
+                if(user != null)
+                {
+                    var result = await _signInManager.PasswordSignInAsync(user, Login.Password, false, false);
+                    if (result.Succeeded)
+                    {
+                        if (string.IsNullOrEmpty(Login.ReturnUrl))
+                        {
+                            return RedirectToAction("Index", "Home");
+                        }
+                        return Redirect(Login.ReturnUrl);
+                    }
+                }
+            
+               ModelState.AddModelError("", "Falha ao realizar Login!");
+               return View("Login");
+        }
+        public  IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(LoginViewModel Register)
+        {
+            if (ModelState.IsValid)
+            {
+                var User = new IdentityUser(){ UserName=Register.UserName, Email = Register.Email };
+                
+                    var result = await _userManager.CreateAsync(User, Register.Password);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Login", "Account");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("Registro", "Falha ao realizar cadastro!");
+                    }
+                
+                
+            }
+            return View(Register);
+        }
+        public async Task<IActionResult> Logout()
+        {
+            HttpContext.Session.Clear();
+            HttpContext.User = null;
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio_Dev.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name ="Nome do Usuário")]
+        [StringLength(255, MinimumLength =10)]
+        public string UserName { get; set; }
+
+        
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        [StringLength(255, MinimumLength = 10)]
+        public string Email { get; set; }
+        [Required]
+        [Display(Name ="Senha")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+       
+        public string ReturnUrl{ get; set; }
+        
+    }
+}
